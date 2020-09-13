92bbd1e45bb5c87792182d6afed76a38e025c418
Fernando Guisbert Segales <guisbertfernando@gmail.com>
2020-09-13 01:50:58 -0400

    Update solution.js

--- Resistor Color/solution.js
+++ Resistor Color/solution.js
@@ -1,10 +1,8 @@
 // 💬 Create an array of resistor color in order
 const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
 
-const colorCode = (color) => {
-    // 💬 Based in 'color' search its index value in 'COLORS' array
-    return COLORS.indexOf(color);
-};
+// 💬 Based in 'color' search its index value in 'COLORS' array
+const colorCode = color => COLORS.indexOf(color);
 
 let result = colorCode("red"); // 🥚 "red"
 console.log(result); // 🐣 2
